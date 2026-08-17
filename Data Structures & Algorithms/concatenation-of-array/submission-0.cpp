7c74c1154a0ec9f79235ef28285c19c5442723dc
CharanSai <72211219c@gmail.com>
2026-08-17 20:06:02 +0530

    Bulk sync: 12 submissions

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-0.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        int n=nums.size();
+        int ans=n+n;
+        vector<int> arr(ans);
+        for(int i=0;i<n;i++){
+            arr[i]=nums[i];
+            arr[i+n]=nums[i];
+        }
+        return arr;
+    }
+};
